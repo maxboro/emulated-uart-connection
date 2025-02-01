8bc7f1d726bbecc217363bfca80884ed6847ea05
Max B. <maksym_borovyk@ukr.net>
2025-02-01 17:13:11 +0200

    Add msg identifications for transmitter

--- transmission/c/transmitter.c
+++ transmission/c/transmitter.c
@@ -9,22 +9,19 @@ int main() {
     printf("main PTY_T: %s\n", params.PTY_T);
     printf("main PTY_R: %s\n", params.PTY_R);
 
-    float rand_val = get_random_float();
-    printf("main rand_val: %f\n", rand_val);
-
-
     int serial_port = open(params.PTY_T, O_WRONLY);
     if (serial_port < 0){
         perror("Error opening serial port");
         return 1;
     }
 
-    char message[] = "String send by C transmitter.\n";
-    for (int n_msg = 0; n_msg < 10; n_msg++){
-        sleep(1);
+    for (int n_msg = 0; n_msg < 10; n_msg++) {
+        char message[40];
+        sprintf(message, "String send by C transmitter #%d.", n_msg);
         write(serial_port, message, strlen(message));
         printf("Msg %d is sent.\n", n_msg);
         fflush(stdout);
+        sleep(1);
     }
     
     close(serial_port);
